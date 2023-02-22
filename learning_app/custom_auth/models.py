0ec73be42058749306b620be6b86233c0f197efd
annett21 <annettarasevich@gmail.com>
2023-02-22 18:04:20 +0300

    added registration endpoint

--- learning_app/custom_auth/models.py
+++ learning_app/custom_auth/models.py
@@ -1,10 +1,22 @@
-from django.db import models
 from django.core.exceptions import ValidationError
-from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+def username_validator(username):
+    """
+    Check if username is unique if not blank.
+    """
+    if not username:
+        if User.objects.filter(username=username).exists():
+            raise ValidationError(
+                _(f"{username} should be unique"),
+                params={'value': username},
+            )
 
 
 class User(models.Model):
-    username = models.CharField(max_length=24, unique=True, blank=True)
+    username = models.CharField(max_length=24, blank=True, validators=[username_validator])
     password = models.CharField(max_length=16, null=True)
     first_name = models.CharField(max_length=16, blank=True)
     last_name = models.CharField(max_length=16, blank=True)
--- learning_app/custom_auth/views.py
+++ learning_app/custom_auth/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from .models import User
+from .serializers import RegistrationSerializer, SimpleUserSerializer
+
+
+class UserViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
+    """
+    A viewset that provides the retrieve and list model actions.
+    """
+
+    queryset = User.objects.all()
+    serializer_class = SimpleUserSerializer
+    permission_classes = (AllowAny,)
+
+
+    @action(methods=['post'], detail=False,
+            url_path='register', url_name='register')
+    def register(self, request):
+        """
+        Sent mail with generated username and password
+        just check is user in db
+        """
+        serializer = RegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        get_object_or_404(User, **serializer.validated_data)
+        return Response(status=status.HTTP_200_OK)
