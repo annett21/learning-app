80a9bde1025bfd3b037168fd86d36e064512c74d
annett21 <annettarasevich@gmail.com>
2023-03-01 19:12:39 +0300

    added sending confirmation email, account activation

--- learning_app/custom_auth/serializers.py
+++ learning_app/custom_auth/serializers.py
@@ -1,7 +1,11 @@
+from django.db.models import Q
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
 from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, Serializer
 
 from .models import User
+from .tokens import account_activation_token
 
 
 class SimpleUserSerializer(ModelSerializer):
@@ -10,10 +14,42 @@ class SimpleUserSerializer(ModelSerializer):
         fields = "__all__"
 
 
-class RegistrationSerializer(ModelSerializer):
+class RegistrationSerializer(Serializer):
     email = serializers.EmailField()
     document_number = serializers.CharField(max_length=16)
+    password = serializers.CharField(max_length=128, write_only=True, required=True)
+    confirmation_password = serializers.CharField(max_length=128, write_only=True, required=True)
 
     class Meta:
-        model = User
-        fields = ["email", "document_number"]
+        fields = ["email", "document_number", "password", "confirmation_password"]
+
+    def validate(self, data):
+        if data["password"] != data["confirmation_password"]:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        
+        data.pop("confirmation_password")
+        if User.objects.filter(
+            (Q(email=data["email"]) & ~Q(document_number=data["document_number"]))
+            | (~Q(email=data["email"]) & Q(document_number=data["document_number"]))
+        ).exists():
+            raise serializers.ValidationError("Wrong email or document number.")
+
+        return data
+    
+
+class ActivateEmailParamsSerializer(Serializer):
+    uidb64 = serializers.CharField()
+    token = serializers.CharField()
+
+    def validate(self, data):
+        uid = force_str(urlsafe_base64_decode(data["uidb64"]))
+        user = User.objects.filter(id=uid).first()
+        if not user:
+            raise serializers.ValidationError({"uidb64": "Unknown user."})
+        
+        if not account_activation_token.check_token(user, data["token"]):
+            raise serializers.ValidationError({"token": "Token expired."})
+        
+        data["user"] = user
+
+        return data
--- learning_app/custom_auth/views.py
+++ learning_app/custom_auth/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.mail import send_mail
-from django.shortcuts import get_object_or_404
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
@@ -9,8 +10,9 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from .models import User
-from .serializers import RegistrationSerializer, SimpleUserSerializer
-from .utils import create_password, create_username
+from .serializers import (ActivateEmailParamsSerializer,
+                          RegistrationSerializer, SimpleUserSerializer)
+from .tokens import account_activation_token
 
 
 class UserViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
@@ -25,17 +27,50 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
     @action(methods=["post"], detail=False)
     def register(self, request):
         """
-        Sending email with temporary username and password if user in db.
+        Check if user in db or create user and send activation link.
         """
         serializer = RegistrationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = get_object_or_404(User, **serializer.validated_data)
-        password = create_password()
-        username = create_username(user.email)
+
+        password = serializer.validated_data.pop("password")
+
+        user, created = User.objects.get_or_create(
+            defaults={"role": User.Role.GUEST}, **serializer.validated_data,
+        )
+
+        if not created and user.is_active:
+            return Response(
+                data="Account already exists",
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user.is_active = True
+        user.set_password(password)
+        user.save()
         send_mail(
-            subject="Credentials for registration",
-            message=f"Here is your temporary password {password} and username {username}. Tap link to change.",
+            subject="Email confirmation",
+            message=(
+                self.reverse_action("activate-email")
+                + f"?uidb64={urlsafe_base64_encode(force_bytes(user.pk))}"
+                + f"&token={account_activation_token.make_token(user)}"
+            ),
             from_email=settings.EMAIL_HOST_USER,
             recipient_list=[user.email],
         )
+
         return Response(status=status.HTTP_200_OK)
+
+
+    @action(methods=["get"], detail=False)
+    def activate_email(self, request):
+        """
+        Change email_confirmed db field if id and token validated.
+        """
+        serializer = ActivateEmailParamsSerializer(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+        user.email_confirmed = True
+        user.save()
+
+        return Response("Thank you for email confirmation!")
