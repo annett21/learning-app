4a5907c42ba1803ce470ec69912828201c8602ec
annett21 <annettarasevich@gmail.com>
2023-03-14 15:45:56 +0300

    added tests for registration

--- learning_app/custom_auth/views.py
+++ learning_app/custom_auth/views.py
@@ -2,7 +2,8 @@ from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.mixins import (ListModelMixin, RetrieveModelMixin,
+                                   UpdateModelMixin)
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
