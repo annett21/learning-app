02d7cbc43b6639a8e4e9eb2e684a6ec8f4de6038
annett21 <annettarasevich@gmail.com>
2023-03-24 15:09:55 +0300

    created course app

--- learning_app/config/urls.py
+++ learning_app/config/urls.py
@@ -36,6 +36,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("custom_auth.urls")),
+    path("course/", include("course.urls")),
     path("login/", views.obtain_auth_token),
     path(
         "",
