ebe804e19efce96c08f57b088c4d487038febec0
annett21 <annettarasevich@gmail.com>
2023-04-04 11:33:12 +0400

    added task viewset for students

--- learning_app/course_task/urls.py
+++ learning_app/course_task/urls.py
@@ -1,9 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import ProfessorTaskViewSet
+from .views import ProfessorTaskViewSet, StudentTaskViewSet
 
 professor_router = routers.SimpleRouter()
 professor_router.register("professor/task", ProfessorTaskViewSet)
 
-urlpatterns = [path("", include(professor_router.urls))]
+student_router = routers.SimpleRouter()
+student_router.register("student/task", StudentTaskViewSet)
+
+urlpatterns = [
+    path("", include(professor_router.urls)),
+    path("", include(student_router.urls)),
+]
--- learning_app/course_task/views.py
+++ learning_app/course_task/views.py
@@ -1,11 +1,59 @@
-from custom_auth.permissions import IsProfessor
+from custom_auth.permissions import IsProfessor, IsStudent
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from .models import Task
 from .serializers import TaskSerializer
 
 
+@method_decorator(
+    name="retrieve",
+    decorator=swagger_auto_schema(
+        operation_description="Returns a taks belonging to professor's courses. "
+        "Can be filtered by course_id."
+    ),
+)
+@method_decorator(
+    name="list",
+    decorator=swagger_auto_schema(
+        operation_description="Returns a list tasks belonging to professor's courses. "
+        "Can be filtered by course_id."
+    ),
+)
+@method_decorator(
+    name="create",
+    decorator=swagger_auto_schema(
+        operation_description="Creates a task with questions."
+    ),
+)
+@method_decorator(
+    name="update",
+    decorator=swagger_auto_schema(
+        operation_description=(
+            "Updates a task. "
+            "Pass question text to create the question. "
+            "Pass question id and text to update the question. "
+            "Pass only question id to delete the question."
+        )
+    ),
+)
+@method_decorator(
+    name="partial_update",
+    decorator=swagger_auto_schema(
+        operation_description=(
+            "Updates a task. "
+            "Pass question text to create the question. "
+            "Pass question id and text to update the question. "
+            "Pass only question id to delete the question."
+        )
+    ),
+)
+@method_decorator(
+    name="destroy",
+    decorator=swagger_auto_schema(operation_description="Deletes a task."),
+)
 class ProfessorTaskViewSet(ModelViewSet):
     queryset = Task.objects.all().order_by("-id")
     serializer_class = TaskSerializer
@@ -13,6 +61,33 @@ class ProfessorTaskViewSet(ModelViewSet):
 
     def get_queryset(self):
         qs = super().get_queryset().filter(course__professor=self.request.user)
+        course_id = self.request.query_params.get("course_id")
+        if course_id:
+            qs = qs.filter(course=course_id)
+        return qs
+
+
+@method_decorator(
+    name="retrieve",
+    decorator=swagger_auto_schema(
+        operation_description="Returns a taks belonging to student's courses. "
+        "Can be filtered by course_id."
+    ),
+)
+@method_decorator(
+    name="list",
+    decorator=swagger_auto_schema(
+        operation_description="Returns a list tasks belonging to students's courses. "
+        "Can be filtered by course_id."
+    ),
+)
+class StudentTaskViewSet(ReadOnlyModelViewSet):
+    queryset = Task.objects.all().order_by("-id")
+    serializer_class = TaskSerializer
+    permission_classes = (IsAuthenticated, IsStudent)
+
+    def get_queryset(self):
+        qs = super().get_queryset().filter(course__students=self.request.user)
         course_id = self.request.query_params.get("course_id")
         if course_id:
             qs = qs.filter(course=course_id)
